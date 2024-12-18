ef8710902f94d234b4a491ec6a215342094d0c31
FredEFREI <frederic.casier@efrei.net>
2024-12-18 16:59:31 +0100

    Multiples enemies type and wave management

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,9 +4,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
-{
-    public GameObject enemyPrefab;
-    public float targetTime = 0f;
+{   
+    private int _maxEnemies = 7;
+    public GameObject enemyTypeA;
+    public GameObject enemyTypeB;
+    public GameObject enemyTypeC;
+    public float nextSpawn = 0f;
+    
+    private float _spawnTime = 7f;
+    private int _difficulty = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +22,69 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetTime -= Time.deltaTime;
-        if (targetTime <= 0)
+        if (Time.time >= 30 && _difficulty == 2)
         {
-            targetTime = 5.0f;
-            var enemy = Instantiate(enemyPrefab);
+            _difficulty = 3;
+            _spawnTime = 6f;
+        }
+        if (Time.time >= 60 && _difficulty == 3)
+        {
+            _difficulty = 4;
+            _spawnTime = 5f;
+        }
+        if (Time.time >= 120 && _difficulty == 4)
+        {
+            _difficulty = 5;
+            _spawnTime = 4f;
+        }
+        
+        if (Time.time >= 160 && _difficulty == 5)
+        {
+            _difficulty = 5;
+            _spawnTime = 3f;
+        }
+        
+        nextSpawn -= Time.deltaTime;
+        if (nextSpawn <= 0)
+        {
+            nextSpawn = _spawnTime;
+
+            if (_maxEnemies > Enemy.nbrEnemies)
+            {
+                switch (Random.Range(0, _difficulty))
+                {
+                    case 0:
+                        Instantiate(enemyTypeA);
+                        Instantiate(enemyTypeA);
+                        Instantiate(enemyTypeA);
+                        break;
+                    case 1:
+                        Instantiate(enemyTypeB);
+                        Instantiate(enemyTypeB);
+                        break;
+                    case 2:
+                        Instantiate(enemyTypeC);
+                        break;
+                    case 3:
+                        Instantiate(enemyTypeA);
+                        Instantiate(enemyTypeB);
+                        Instantiate(enemyTypeB);
+                        Instantiate(enemyTypeA);
+                        break;
+                    case 4:
+                        Instantiate(enemyTypeC);
+                        Instantiate(enemyTypeC);
+                        break;
+                    default:
+                        Instantiate(enemyTypeA);
+                        Instantiate(enemyTypeA);
+                        Instantiate(enemyTypeB);
+                        Instantiate(enemyTypeB);
+                        Instantiate(enemyTypeC);
+                        break;
+                }
+            }
+
         }
     }
 }
